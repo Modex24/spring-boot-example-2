75fd99a163bf45c7f10349682d3d7044b4aa7482
Mkdesk <babiegold@gmail.com>
2023-06-19 14:45:45 +0000

    first commit

---
+++ src/main/java/com/example/springbootexample2/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.springbootexample2.repository;
+
+import com.example.springbootexample2.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>{
+    Product findByName(String name);
+}
